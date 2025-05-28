827af705ee348795b1491f3adbcf0037283adcfe
Luca Baldini <luca.baldini@pi.infn.it>
2025-05-28 12:04:41 +0200

    Fixed unit tests.

--- tests/test_fmt.py
+++ tests/test_fmt.py
@@ -17,15 +17,9 @@
 """Unit tests for the fmt.py module.
 """
 
-import binascii
-import os
-import tempfile
 import time
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-from astropix_analysis.fmt import BitPattern
+from astropix_analysis.fmt import BitPattern, AstroPix4Readout
 
 
 # Mock data from a small test run with AstroPix4---the bytearray below should
@@ -38,7 +32,6 @@ sample_readout_data = bytearray.fromhex('bcbce08056e80da85403bcbcbcbcbcbcbcbce08
 decoded_header = 'dec_ord,id,payload,row,col,ts1,tsfine1,ts2,tsfine2,tsneg1,tsneg2,tstdc1,tstdc2,ts_dec1,ts_dec2,tot_us'
 decoded_data0 = (0,7,0,5,5167,3,5418,6,1,0,0,0,49581,52836,162.75)
 decoded_data1 = (0,7,0,5,6124,2,4876,5,0,1,0,0,54716,61369,332.65)
-
 
 
 def test_bit_pattern():
@@ -61,25 +54,18 @@ def test_bit_pattern():
     assert pattern[6:10] == 3
 
 
-def _test_original_decoding():
-    """Test that our poor-man copy of the original decode returns the same thing
-    we found into the csv file.
-    """
-    df = original_decode(sample_readout_data)
-    print(df)
-    assert tuple(df.iloc[0]) == decoded_data0
-    assert tuple(df.iloc[1]) == decoded_data1
-
-
-def _test_new_decoding():
+def test_new_decoding():
     """Test the new decoding stuff.
     """
-    readout = AstroPixReadout(sample_readout_data, trigger_id=0, timestamp=time.time_ns())
+    trigger_id = 0
+    timestamp = time.time_ns()
+    readout = AstroPix4Readout(trigger_id, timestamp, sample_readout_data)
     print(readout)
-    assert readout.num_hits() == 2
-    for hit in readout.hits:
+    hits = readout.decode()
+    assert len(hits) == 2
+    for hit in hits:
         print(hit)
-    hit0, hit1 = readout.hits
+    hit0, hit1 = hits
     # Compare the hit objects with the conten of the csv files---note we are
     # assuming that if the TOT value in us is ok, then all the intermediate timestamp
     # fields are ok, as well.
@@ -88,104 +74,3 @@ def _test_new_decoding():
     assert (hit1.chip_id, hit1.payload, hit1.row, hit1.column) == decoded_data1[0:4]
     assert hit1.tot_us == decoded_data1[-1]
 
-
-def _test_file_header():
-    """Test the file header.
-    """
-    # Create a dummy header.
-    header = FileHeader(dict(version=1, content='hits'))
-    print(header)
-
-    # Write the header to an output file.
-    kwargs = dict(suffix=AstroPixBinaryFile._EXTENSION, delete_on_close=False, delete=True)
-    with tempfile.NamedTemporaryFile('wb', **kwargs) as output_file:
-        print(f'Writing header to {output_file.name}...')
-        header.write(output_file)
-        output_file.close()
-
-        # Read back the header from the output file.
-        print(f'Reading header from {output_file.name}...')
-        with open(output_file.name, 'rb') as input_file:
-            twin = FileHeader.read(input_file)
-        print(twin)
-
-    # Make sure that the whole thing roundtrips.
-    assert twin == header
-
-
-def _test_file():
-    """Try writing and reading a fully-fledged output file.
-    """
-    # Create a dummy header.
-    header = FileHeader(dict(version=1, content='hits'))
-    print(header)
-    # Grab our test AstroPix4 hits.
-    readout = AstroPixReadout(sample_readout_data, trigger_id=0, timestamp=time.time_ns())
-
-    # Write the output file.
-    kwargs = dict(suffix=AstroPixBinaryFile._EXTENSION, delete_on_close=False, delete=True)
-    with tempfile.NamedTemporaryFile('wb', **kwargs) as output_file:
-        print(f'Writing data to {output_file.name}...')
-        header.write(output_file)
-        for hit in readout.hits:
-            hit.write(output_file)
-        output_file.close()
-
-        # Read back the input file---note this is done in the context of the first
-        # with, so that tempfile can cleanup after the fact.
-        print(f'Reading data from {output_file.name}...')
-        with AstroPixBinaryFile(AstroPix4Hit).open(output_file.name) as input_file:
-            print(input_file.header)
-            for i, hit in enumerate(input_file):
-                print(hit)
-                assert hit == readout.hits[i]
-
-
-def _test_playback(num_hits: int = 10):
-    """Test the full playback of a real file.
-    """
-    file_path = os.path.join(os.path.dirname(__file__), 'data', '20250205_094324_data.apx')
-    with AstroPixBinaryFile(AstroPix4Hit).open(file_path) as input_file:
-        print(f'\nStarting playback of binary file {file_path}...')
-        print(f'File header: {input_file.header}')
-        for i, hit in enumerate(input_file):
-            if i < num_hits:
-                print(hit)
-            elif i == num_hits:
-                print('...')
-        print(f'{i + 1} hits found')
-
-
-def _test_plot_file():
-    """Basic test plotting the content of the sample binary file.
-    """
-    file_path = os.path.join(os.path.dirname(__file__), 'data', '20250205_094324_data.csv')
-    chip_id, payload, row, column, ts_neg1, ts_coarse1, ts_fine1, ts_tdc1, ts_neg2, \
-        ts_coarse2, ts_fine2, ts_tdc2, ts_dec1, ts_dec2, tot_us, trigger_id, timestamp = \
-        np.loadtxt(file_path, delimiter=',', unpack=True)
-    dt = np.diff(timestamp) / 1.e6
-
-    plt.figure('TOT')
-    plt.hist(tot_us, bins=25)
-    plt.xlabel('TOT [$\\mu$s]')
-
-    plt.figure('Time differences')
-    plt.hist(dt, bins=25)
-    plt.xlabel('$\\Delta$T [ms]')
-
-
-def _test_csv_convert():
-    """Read a sample .apx file and convert it to csv.
-    """
-    file_path = os.path.join(os.path.dirname(__file__), 'data', '20250205_094324_data.apx')
-    kwargs = dict(suffix='.csv', delete_on_close=False, delete=True)
-    with tempfile.NamedTemporaryFile('w', **kwargs) as output_file:
-        output_file.close()
-        print(f'Converting {file_path} to {output_file.name}...')
-        out = apxdf_to_csv(file_path, AstroPix4Hit, output_file_path=output_file.name)
-        assert out == output_file.name
-
-
-if __name__ == '__main__':
-    test_plot_file()
-    plt.show()
