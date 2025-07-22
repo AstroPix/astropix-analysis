076919033db85fc24738a7f83e597a341d570f19
Luca BALDINI <lbaldini@desktop.pi.infn.it>
2025-07-22 14:46:45 +0200

    made change so we can process a run with more than 1 vpdac, fixed problems in that version

--- Injection_Scan_Data_Reduction.py
+++ Injection_Scan_Data_Reduction.py
@@ -33,39 +33,39 @@ def Get_Threshold_Counts_Array(VPDAC, TuneDAC, column):
 
     return threshold_counts_array
 
-top_directory=args.input.replace('\\','/')
-number_of_thresholds=len(extension_finder('csv',top_directory+'/TuneDAC_0/Col_0'))
-VPDAC_Threshold_Counts_Array=np.zeros((8,13,number_of_thresholds,2))
-for TuneDAC in np.arange(8):
-    for col in np.arange(13):
-        folder_name=top_directory+f'/TuneDAC_{TuneDAC}/Col_{col}'
-        threshold_counts_list=[]
-        file_list=extension_finder('.csv',folder_name)
-        for file_name in file_list:
-            threshold=int(file_name.split('/')[-1].split('_')[1].rstrip('mV'))
-            counts=count_lines(file_name)-1
-            threshold_counts_list.append([threshold,counts])
-        threshold_counts_array=np.array(threshold_counts_list)
-        threshold_counts_array=threshold_counts_array[np.argsort(threshold_counts_array[:,0])]
-        VPDAC_Threshold_Counts_Array[TuneDAC,col]=threshold_counts_array
-
-np.savetxt(args.output+'.txt',VPDAC_Threshold_Counts_Array.flatten())
-
 # top_directory=args.input.replace('\\','/')
-# number_of_thresholds=len(extension_finder('csv',top_directory+'/VPDAC_0/TuneDAC_0/Col_0'))
-# Full_Threshold_Counts_Array=np.zeros((4,8,13,number_of_thresholds,2))
-# for VPDAC in [10,20,30,40]:
-#     for TuneDAC in np.arange(8):
-#         for col in np.arange(13):
-#             folder_name=top_directory+f'/VPDAC_{VPDAC}/TuneDAC_{TuneDAC}/Col_{col}'
-#             threshold_counts_list=[]
-#             file_list=extension_finder('.csv',folder_name)
-#             for file_name in file_list:
-#                 threshold=int(file_name.split('/')[-1].split('_')[1].rstrip('mV'))
-#                 counts=count_lines(file_name)-1
-#                 threshold_counts_list.append([threshold,counts])
-#             threshold_counts_array=np.array(threshold_counts_list)
-#             threshold_counts_array=threshold_counts_array[np.argsort(threshold_counts_array[:,0])]
-#             Full_Threshold_Counts_Array[VPDAC,TuneDAC,col]=threshold_counts_array
+# number_of_thresholds=len(extension_finder('csv',top_directory+'/TuneDAC_0/Col_0'))
+# VPDAC_Threshold_Counts_Array=np.zeros((8,13,number_of_thresholds,2))
+# for TuneDAC in np.arange(8):
+#     for col in np.arange(13):
+#         folder_name=top_directory+f'/TuneDAC_{TuneDAC}/Col_{col}'
+#         threshold_counts_list=[]
+#         file_list=extension_finder('.csv',folder_name)
+#         for file_name in file_list:
+#             threshold=int(file_name.split('/')[-1].split('_')[1].rstrip('mV'))
+#             counts=count_lines(file_name)-1
+#             threshold_counts_list.append([threshold,counts])
+#         threshold_counts_array=np.array(threshold_counts_list)
+#         threshold_counts_array=threshold_counts_array[np.argsort(threshold_counts_array[:,0])]
+#         VPDAC_Threshold_Counts_Array[TuneDAC,col]=threshold_counts_array
 
-# np.savetxt(args.output+'.txt',Full_Threshold_Counts_Array.flatten())
+# np.savetxt(args.output+'.txt',VPDAC_Threshold_Counts_Array.flatten())
+
+top_directory=args.input.replace('\\','/')
+number_of_thresholds=len(extension_finder('csv',top_directory+'/VPDAC_10/TuneDAC_0/Col_0'))
+Full_Threshold_Counts_Array=np.zeros((4,8,13,number_of_thresholds,2))
+for i_VPDAC,VPDAC in enumerate([10,20,30,40]):
+    for TuneDAC in np.arange(8):
+        for col in np.arange(13):
+            folder_name=top_directory+f'/VPDAC_{VPDAC}/TuneDAC_{TuneDAC}/Col_{col}'
+            threshold_counts_list=[]
+            file_list=extension_finder('.csv',folder_name)
+            for file_name in file_list:
+                threshold=int(file_name.split('/')[-1].split('_')[1].rstrip('mV'))
+                counts=count_lines(file_name)-1
+                threshold_counts_list.append([threshold,counts])
+            threshold_counts_array=np.array(threshold_counts_list)
+            threshold_counts_array=threshold_counts_array[np.argsort(threshold_counts_array[:,0])]
+            Full_Threshold_Counts_Array[i_VPDAC,TuneDAC,col]=threshold_counts_array
+
+np.savetxt(args.output+'.txt',Full_Threshold_Counts_Array.flatten())
